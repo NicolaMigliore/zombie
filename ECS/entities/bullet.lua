a505f5ef5d9181f96007fa0f852ed8b3778c69f4
n.migliore <n.migliore@blogic.it>
2024-08-09 18:09:23 +0200

    reduced characters

--- ECS/entities/bullet.lua
+++ ECS/entities/bullet.lua
@@ -1,58 +1,50 @@
--- create new bullet
-function spawn_bullet(_x,_dir)
-    -- set bullet position and speed
-    local bullet_speed = _dir*2
-    _x = _dir > 0 and _x+10 or _x-2
+function spawn_bullet(x,d)
+    local spd=d*2
+    x=d>0 and x+10 or x-2
 
     local bullet_states = {
-        travel = function(_e)
-            -- check for collision with zombie
-            local bullet_collisions = physics_system.collisions[_e.id]
-            if bullet_collisions != nil then
-                local oi = 1
-                local hit_zombie = false
-                while oi <= #bullet_collisions and hit_zombie == false do
-                    local ent = get_entity(bullet_collisions[oi])
-                    if(ent.kind=="zombie") hit_zombie = true return "hit"
-                    oi += 1
+        travel = function(e)
+            local c = physics_system.collisions[e.id]
+            if c then
+                for i=1,#c do
+                    local ent = get_entity(c[i])
+                    if ent.kind=="zombie" then
+                        return "hit"
+                    end
                 end
             end
-            -- keep traveling
             return "travel"
         end,
-        hit = function(_e)
+        hit = function(e)
             sfx(14)
             return "_death"
         end,
-        _death = function(_e)
-            del(entities,_e)
+        _death = function(e)
+            del(entities,e)
         end,
     }
 
     local bullet_hitboxes = {
-        hit = { ox=0, oy=0, w=2, h=1 }
+        hit = {ox=0,oy=0,w=2,h=1}
     }
 
-    -- #region bullet entity
-    local new_bullet = new_entity({
-        kind = "bullet",
-        code = "bulle",
-        position = new_position(_x,68,2,1),
-        sprite = new_sprite({sprites = {{x=8,y=96,w=2,h=1,ox=0,oy=0,fx=false,fy=false}}, pal_rep={{10,6}}}),
-        intention = new_intention(),
-        control = new_control({spd_x = bullet_speed, control_func = bullet_control}),
-        state = new_state(bullet_states, "travel"),
-        battle = new_battle(bullet_hitboxes,{},{health=100, damage=70}),
-        collider = new_collider(0,0,2,1,{is_solid=false}),
-    })
-    add(entities, new_bullet)
+    add(entities,new_entity({
+        kind="bullet",
+        code="bulle",
+        position=new_position(x,68,2,1),
+        sprite=new_sprite({sprites={{x=8,y=96,w=2,h=1,ox=0,oy=0,fx=false,fy=false}},pal_rep={{10,6}}}),
+        intention=new_intention(),
+        control=new_control({spd_x=spd,control_func=bullet_control}),
+        state=new_state(bullet_states,"travel"),
+        battle=new_battle(bullet_hitboxes,{},{health=100,damage=70}),
+        collider=new_collider(0,0,2,1,{is_solid=false}),
+    }))
 end
 
--- control bullet movement
-function bullet_control(_e)
-    -- move bullet
-    _e.position.x += _e.control.spd_x
-
-    -- delete bullet after a certain distance
-    if(abs(_e.position.x - player.position.x)>130) _e.state.previous = _e.state.current _e.state.current = "_death"
+function bullet_control(e)
+    e.position.x += e.control.spd_x
+    if abs(e.position.x-player.position.x)>130 then
+        e.state.previous=e.state.current
+        e.state.current="_death"
+    end
 end
